6d573bda0b9d5368df35cfb219ed5fc743f77c98
RoshanCD2007 <81854107+RoshanCD2007@users.noreply.github.com>
2021-07-13 16:58:44 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAxoCjUCtQNmXQuyYz-LSALC6SZ4ZXvj-c",
+    authDomain: "kwitter-project-93134.firebaseapp.com",
+    databaseURL: "https://kwitter-project-93134-default-rtdb.firebaseio.com",
+    projectId: "kwitter-project-93134",
+    storageBucket: "kwitter-project-93134.appspot.com",
+    messagingSenderId: "896496638941",
+    appId: "1:896496638941:web:704f08f92d7021ea0e7434"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
